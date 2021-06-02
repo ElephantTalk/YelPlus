18e879d3ae65ab4fe39ce9f5002dfdbf6457b4e3
ibrahim-ali00 <ibrahim.ali00@protonmail.com>
2021-06-02 08:44:17 -0700

    heroku setup

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Root from "./components/root";
-import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<h1>Welcome to YelPlus!</h1>, root);
 });
